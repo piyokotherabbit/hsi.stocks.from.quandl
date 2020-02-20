1cb73ce77cbdf25d800c76f05a65ac51b108b26b
piyokotherabbit <u.yanaka@icloud.com>
2020-02-20 13:41:24 +0900

    Update HSISTOCKS.R

--- HSISTOCKS.R
+++ HSISTOCKS.R
@@ -6,7 +6,7 @@ install.packages("pbapply")
 library(pbapply)
 
 
-Quandl.api_key("QdYxxsuDbVdE-hkHvhcV")
+Quandl.api_key("YOUR_QUANDL_API_KEY_GOES_HERE")
 
 HSI.COMP_temp <- c(700,1299,5,939,2318,941,1398,388,3988,883,1,823,2,3,2628,1113,16,11,27,1928,386,688,2388,669,1109,2382,2313,1093,2319,6,2007,857,1177,175,66,288,267,17,12,1997,1088,1038,3328,1044,762,2018,151,19,83,101)
 HSI.COMP <- formatC(HSI.COMP_temp, width = 5, flag = "0")
