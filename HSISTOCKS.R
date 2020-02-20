b27a4340701535a6a77e28726411da3267db4374
Yuto Yanaka <ynk@Yuto-no-MacBook-Pro-2.local>
2020-02-20 14:47:10 +0900

    added some functions

--- HSISTOCKS.R
+++ HSISTOCKS.R
@@ -5,12 +5,35 @@ library(dplyr)
 install.packages("pbapply")
 library(pbapply)
 
-
+#Loading my quandl api key
 Quandl.api_key("QdYxxsuDbVdE-hkHvhcV")
 
+#Loading the code for each HSI consitutients 
 HSI.COMP_temp <- c(700,1299,5,939,2318,941,1398,388,3988,883,1,823,2,3,2628,1113,16,11,27,1928,386,688,2388,669,1109,2382,2313,1093,2319,6,2007,857,1177,175,66,288,267,17,12,1997,1088,1038,3328,1044,762,2018,151,19,83,101)
 HSI.COMP <- formatC(HSI.COMP_temp, width = 5, flag = "0")
 HSI.COMP.LIST <- sapply(HSI.COMP,function(x)paste("HKEX/", HSI.COMP, sep = ""))
 
+#Aquiring datasets from quandl
 HSI.STOCKS <- pblapply(HSI.COMP.LIST[1:50], Quandl)
 names(HSI.STOCKS) <- HSI.COMP.LIST[1:50]
+
+#Scaling every each numbers
+for (i in 1:50) {
+  
+  HSI.STOCKS[[i]]$`Net Change`<- NULL
+  HSI.STOCKS[[i]]$`Change (%)`<- NULL
+  HSI.STOCKS[[i]]$Bid <- NULL
+  HSI.STOCKS[[i]]$Ask <- NULL
+  HSI.STOCKS[[i]]$`P/E(x)`<- NULL
+  HSI.STOCKS[[i]]$`Previous Close`<- NULL
+  HSI.STOCKS[[i]]$`Lot Size` <- NULL
+  HSI.STOCKS[[i]]$Date <- as.character(HSI.STOCKS[[i]]$Date)
+  
+  HSI.STOCKS[[i]]$`Nominal Price` <- scale(HSI.STOCKS[[i]]$`Nominal Price`)
+  HSI.STOCKS[[i]]$High <- scale(HSI.STOCKS[[i]]$High)
+  HSI.STOCKS[[i]]$Low <- scale(HSI.STOCKS[[i]]$Low)
+  HSI.STOCKS[[i]]$`Share Volume (000)` <- scale(HSI.STOCKS[[i]]$`Share Volume (000)`)
+  HSI.STOCKS[[i]]$`Turnover (000)`<- scale(HSI.STOCKS[[i]]$`Turnover (000)`)
+}
+
+
